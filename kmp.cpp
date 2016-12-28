29beb96c7c3ff6b94e24e33be77df8ebbcc76599
Michael Colson <mbcolson@yahoo.com>
2016-12-28 00:06:21 -0800

    Create kmp.cpp

---
+++ kmp.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Knuth-Morris-Pratt String Searching Algorithm
+// Searches for occurrences of a string (pattern) in text
+
+#define MAX 500100
+string text, pat;
+int b[MAX], n, m;
+
+void KMP()
+{
+    int i = 0, j = -1;
+    b[0] = -1;
+    while(i < m)
+    {
+        while(j >= 0 && pat[i] != pat[j])
+            j = b[j];
+        i++; j++;
+        b[i] = j;
+    }    
+    i = 0, j = 0;
+    while(i < n)
+    {
+        while(j >= 0 && text[i] != pat[j])
+            j = b[j];
+        i++; j++;
+        if(j == m)
+        {
+            cout << "Found pattern at " << i - j << endl;
+            j = b[j];
+        }
+    }    
+}   
+
+int main()
+{
+    text = "Hello world, how are you doing today?";
+    pat = "world";
+    n = text.length();
+    m = pat.length();
+    KMP();  // output: "Found pattern at 6"
+    return 0;
+}
