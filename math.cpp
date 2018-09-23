37265c773db058e218daee67b0e69fa7f73ccc08
Michael Colson <mbcolson@yahoo.com>
2018-09-22 22:00:06 -0700

    Update math.cpp

--- math.cpp
+++ math.cpp
@@ -3,6 +3,18 @@ using namespace std;
 
 typedef long long ll;
 
+ll gcd(ll a, ll b)
+{
+    if(b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+ll lcm(ll a, ll b)
+{
+    return a * (b / gcd(a, b));   
+}
+
 // Returns true if n is prime and false otherwise
 bool isPrime(ll n)
 {
