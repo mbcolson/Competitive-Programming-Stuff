6ae7c67b8000ec692c82063ddf52336b9073c3b0
Michael Colson <mbcolson@yahoo.com>
2023-04-20 00:31:35 -0700

    Update sieve.cpp

--- sieve.cpp
+++ sieve.cpp
@@ -5,27 +5,25 @@ using namespace std;
 vector<bool> prime(MAX, true);
 
 // Sieve of Eratosthenes: finds all primes up to a limit
-void Sieve()
-{
+void Sieve() {
     prime[0] = prime[1] = false;
-    for(int i = 2; i * i < prime.size(); i++)
-    {
-        if(prime[i])
-        {
+    
+    for(int i = 2; i * i < prime.size(); i++) {
+        if(prime[i]) {
             for(int j = i * i; j < prime.size(); j += i)
                 prime[j] = false;
         }
     }
 }
 
-int main()
-{   
+int main() {   
     Sieve();
+    
     // print all primes in the range (0 - 100)
-    for(int i = 0; i <= 100; i++)
-    {
+    for(int i = 0; i <= 100; i++) {
         if(prime[i])
             cout << i << endl;
-    }    
+    }
+    
     return 0;
 }
