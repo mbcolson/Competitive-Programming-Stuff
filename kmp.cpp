7a6c6708e4a164b372d4d8dba531abb13e282752
Michael Colson <mbcolson@yahoo.com>
2023-04-20 00:21:53 -0700

    Update kmp.cpp

--- kmp.cpp
+++ kmp.cpp
@@ -8,12 +8,11 @@ using namespace std;
 int b[MAX];  // contains the longest prefix that is also a proper suffix of the pattern
              // For example, if the pattern is "ABAAABAB", b = [-1, 0, 0, 1, 1, 1, 2, 3, 2]
 
-void compute_prefix(string pat)
-{
+void compute_prefix(string pat) {
     int m = pat.length(), k = -1;
     b[0] = -1;
-    for(int q = 0; q < m; q++)
-    {
+  
+    for(int q = 0; q < m; q++) {
         while(k >= 0 && pat[k] != pat[q])
             k = b[k];
         k++;
@@ -22,26 +21,25 @@ void compute_prefix(string pat)
 }    
 
 // Search for a pattern in the text string. If found, print the index where the pattern was found.
-void kmp(string text, string pat)
-{
+void kmp(string text, string pat) {
     int n = text.length(), m = pat.length();
     compute_prefix(pat);
     int q = 0;
-    for(int i = 0; i < n; i++)
-    {
+  
+    for(int i = 0; i < n; i++) {
         while(q >= 0 && pat[q] != text[i])
             q = b[q];
+      
         q++;
-        if(q == m)
-        {    
+      
+        if(q == m) {    
             cout << "Found pattern at " << i - q + 1 << endl;
             q = b[q];
         }
     }    
 }    
 
-int main()
-{   
+int main() {   
     string text = "ABAAABAABBBABABABBB";
     string pat = "ABAB";
     kmp(text, pat);     /* output: "Found pattern at 11"
