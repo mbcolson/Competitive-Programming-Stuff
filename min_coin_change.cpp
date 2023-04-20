17b853a11b0aec9d76bf9c81b6b17de51265af02
Michael Colson <mbcolson@yahoo.com>
2023-04-20 00:30:32 -0700

    Update min_coin_change.cpp

--- min_coin_change.cpp
+++ min_coin_change.cpp
@@ -3,12 +3,14 @@ using namespace std;
 
 // This function returns the minimum number of coins to make change for a given amount.
 // Note: If it's impossible to make change for the amount, it will return -1 
-int minCoins(vector<int>& den, int amount)
-{
+int minCoins(vector<int>& den, int amount) {
     int dp[amount + 1];
+    
     for(int i = 0; i < amount + 1; i++)
         dp[i] = INT_MAX - 1;
+    
     dp[0] = 0;
+    
     for(int i = 1; i <= amount; i++)
         for(int j = 0; j < den.size(); j++) 
             if(i - den[j] >= 0)
@@ -16,13 +18,13 @@ int minCoins(vector<int>& den, int amount)
 
     if(dp[amount] == (INT_MAX - 1))
         return -1;
+    
     return dp[amount];
 }    
 
-int main() 
-{
+int main() {
     vector<int> den = {1, 5, 10, 21, 25};   // coin denominations 
-    cout << minCoins(den, 63);              // output: 3 
+    cout << minCoins(den, 63) << endl;      // output: 3 
     
     return 0;
 }
