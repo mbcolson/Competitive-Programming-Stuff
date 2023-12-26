e5963e2b6bd895176c893d8c7c443650e5bcb17c
Michael Colson <mbcolson@yahoo.com>
2023-12-26 01:51:16 -0800

    Update 2d_vector_rotate.cpp

--- 2d_vector_rotate.cpp
+++ 2d_vector_rotate.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-/* Rotates the 2d vector 90 degress clockwise */
+/* Rotates a 2d vector 90 degress clockwise */
 vector<vector<int>> rotate(vector<vector<int>>& v) {
     int n = v.size(), m = v[0].size();
     vector<vector<int>> res(m, vector<int>(n, 0));
@@ -27,9 +27,9 @@ int main() {
         cout << endl;
     }
 
-   /*  [7 4 1]        [9 8 7]         [3 6 9]         [1 2 3]
-       [8 5 2]  -->   [6 5 4]   -->   [2 5 8]   -->   [4 5 6]
-       [9 6 3]        [3 2 1]         [1 4 7]         [7 8 9]   */
+   /*  [1 2 3]        [7 4 1]        [9 8 7]         [3 6 9]         [1 2 3]
+       [4 5 6]  -->   [8 5 2]  -->   [6 5 4]   -->   [2 5 8]   -->   [4 5 6]
+       [7 8 9]        [9 6 3]        [3 2 1]         [1 4 7]         [7 8 9]   */
   
     return 0;
 }
